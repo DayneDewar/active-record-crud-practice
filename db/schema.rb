1735f7c95873e475e8f50b0996635903187fe2d8
Dayne Dewar <daynedewar@yahoo.com>
2021-01-21 10:40:16 -0500

    likes plants

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_21_150302) do
+ActiveRecord::Schema.define(version: 2021_01_21_153503) do
 
   create_table "people", force: :cascade do |t|
     t.string "name"
+    t.boolean "likes_plants?"
   end
 
   create_table "plants", force: :cascade do |t|
